a9930cda873ef455d954b5d8dbebdaa2a9feea03
Mariia20222022 <mariaorlova1818@gmail.com>
2023-03-13 00:00:58 +0200

    changes

---
+++ src/index.js
@@ -0,0 +1,112 @@
+import './css/styles.css';
+import { searchImages } from './search-images';
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+const searchForm = document.querySelector(`.search-form`);
+const loadMoreButton = document.querySelector(`.load-more `);
+const gallery = document.querySelector(`.gallery`);
+searchForm.addEventListener(`submit`, onSearch);
+loadMoreButton.addEventListener(`click`, onLoadMore);
+loadMoreButton.style.display = `none`;
+let searchQuery = ``;
+
+let page = 1;
+const lightbox = new SimpleLightbox('.gallery a');
+const perPage = 40;
+function onSearch(event) {
+  event.preventDefault();
+  gallery.innerHTML = ``;
+  searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+  page = 1;
+  //  searchImages(searchQuery, page, perPage)
+  if (searchQuery !== ``) {
+    searchImages(searchQuery, page, perPage).then(data => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`
+        );
+      }
+      renderPhoto(data.hits);
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      lightbox.refresh();
+      loadMoreButton.style.display = `block`;
+    });
+  }
+}
+
+function onLoadMore() {
+  page += 1;
+  // loadMoreButton.classList.remove(`hidden`);
+  searchImages(searchQuery, page, perPage).then(data => {
+    if (data.totalHits === 0) {
+      alert(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+    } else {
+      renderPhoto(data.hits);
+
+      // alert(`Hooray! We found ${data.totalHits} images.`);
+      const totalPages = Math.ceil(data.totalHits / perPage);
+      if (page > totalPages) {
+        Notiflix.Notify.warning(
+          `We're sorry, but you've reached the end of search results.`
+        );
+      }
+    }
+    loadMoreButton.style.display = 'block';
+  });
+  // if (data.total === data.totalHits) {
+
+  // handleNextGroupLoaded();
+}
+
+function renderPhoto(images) {
+  const markup = images
+    .map(img => {
+      const {
+        id,
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = img;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// function smoothScrollToNextGroup() {
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
+
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: 'smooth',
+//   });
+// }
+
+// function handleNextGroupLoaded() {
+//   requestAnimationFrame(() => {
+//     smoothScrollToNextGroup();
+//   });
+// }
---
+++ src/search-images.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+export { searchImages };
+
+async function searchImages(searchQuery, page, perPage) {
+  const apiKey = `34197852-03b7352fc2ee661f306011b94`;
+  const BASE_URL = `https://pixabay.com/api/`;
+
+  const response = await axios.get(`${BASE_URL}?key=${apiKey}`, {
+    params: {
+      q: searchQuery,
+      perPage: 40,
+      // page: page,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+  if (response.status !== 200) {
+    throw new Error(response.status);
+  }
+  const images = await response.data;
+  return images;
+}
+
+// const response = await axios.get(`${BASE_URL}?key=${API_KEY}`, {
+//     params: {
+//       q: searchword,
+//       per_page: 40,
+//       page: page,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//     },
